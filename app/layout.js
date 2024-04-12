2292334805b59b3a8b0f28cb171ea799fb885d7c
Ronak <ronakasnani5@gmail.com>
2024-04-13 00:41:31 +0530

    initial commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +12,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="light">
+      <body
+        className={cn(
+          `min-h-screen font-sans antialiased grainy`,
+          inter.className
+        )}
+      >
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
